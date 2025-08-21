c2ff826109933e21aaba94e73b08522bf0fc9360
dkgusrn <gih1214@hanmail.net>
2025-08-21 12:23:56 +0900

    config db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
+  local: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
